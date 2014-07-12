52c3ccb89b4a620e096b98fe0dc3bac9283de937
elliotthilaire <elliott.hilaire@gmail.com>
2014-07-12 17:06:59 +1000

    added seed.rb

---
+++ seed.rb
@@ -0,0 +1,10 @@
+
+
+require_relative 'app.rb'
+
+Report.create(
+   	description: 'sample description',
+   	latitude: '-10', #Replace this with a real sample
+   	longitude: '-10', 
+   	contact: 'person@example.com'
+)
