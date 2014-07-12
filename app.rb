10c5ef77bb744ec9721793bde875d35d4303af44
elliotthilaire <elliott.hilaire@gmail.com>
2014-07-12 12:37:03 +1000

    added datamapper to app.rb

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'data_mapper'
+
 
 get '/hi' do
   "Hello World!"
 end
+
+
+# This is required, do not remove.
+# It tells datamapper that there are no more models to load.
+DataMapper.finalize
+
+# DataMapper.auto_migrate!
+DataMapper.auto_upgrade!
