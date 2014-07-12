e56ce6ad6628ab5ef823989fccde91d12e741463
elliotthilaire <elliott.hilaire@gmail.com>
2014-07-12 15:31:55 +1000

    added carrierwave

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby "2.0.0"
 gem "sinatra"
 gem "data_mapper"
 gem 'dm-sqlite-adapter'
+gem "carrierwave-datamapper"
 
 group :development, :test do
   gem 'shotgun'
--- app.rb
+++ app.rb
@@ -1,15 +1,20 @@
 require 'sinatra'
 require 'data_mapper'
 require 'dm-sqlite-adapter'
+require 'carrierwave/datamapper'
 
 require_relative 'configure'
 
 ## This is an example model.
-class Post
+class Report
   include DataMapper::Resource
   property :id, Serial
-  property :title, String
-  property :body, Text
+  property :description, String
+  property :photo_url, String
+  property :latitude, Float
+  property :longitude, Float
+  property :contact, String
+  property :status, Enum[ :new, :open, :closed, :invalid ], default: :open
   property :created_at, DateTime
 end
 
